20be46602842b31b5b47638b4c974c992ad537cc
= <julian31186@gmail.com>
2024-09-01 01:15:00 -0400

    .

---
+++ lc/3276.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxScore(self, grid: List[List[int]]) -> int:
+        arr = [[] for i in range(100)]
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                arr[grid[i][j] - 1].append(i)
+        
+        @cache
+        def dfs(idx,mask):
+            if idx == 100: return 0
+            res = 0
+            for i in range(len(arr[idx])):
+                if (1 << arr[idx][i]) & mask == 0:
+                    res = max(res,idx + 1 + dfs(idx + 1, mask | (1 << arr[idx][i])))
+            res = max(res, dfs(idx + 1, mask))
+            return res
+        
+        return dfs(0,0)
