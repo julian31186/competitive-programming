ab5cb0d06a11b7f6ba0d7daab1ff6e4605b3bf4b
= <julian31186@gmail.com>
2024-11-12 14:15:07 -0500

    update Sieve template

--- templates/sieve.py
+++ templates/sieve.py
@@ -6,6 +6,11 @@ first_n_primes(n)
 - Returns the first n primes up to N passed into Sieve constructor if there are n primes
 - Returns [] if there are NOT n primes that exist in the range [2,N]
 
+get_primes()
+- Returns list of primes between [2,N]
+
+is_prime(n)
+- Checks if n is a prime number in O(1) (If n is within the range [0,N]) 
 
 TC: O(n * log log n)
 SC: O(N)
@@ -14,18 +19,23 @@ from math import isqrt
 
 class Sieve():
     def __init__(self,N=10**7):
+        self.is_p = [True] * (N + 1)
+        self.is_p[0] = False
+        self.is_p[1] = False
         self.primes = self._sieve(N + 1)
-
-    def _sieve(self,N):
-        is_p = [True] * N
-        is_p[0] = False
-        is_p[1] = False
         
+    def _sieve(self,N):
         for i in range(isqrt(N - 1) + 1):
-            if i >= 2 and is_p[i]:
+            if i >= 2 and self.is_p[i]:
                 for j in range(i**2,N,i):
-                    is_p[j] = False
-        return [n for n in range(N) if is_p[n]]
+                    self.is_p[j] = False
+        return [n for n in range(N) if self.is_p[n]]
+
+    def get_primes(self):
+        return self.primes
+
+    def is_prime(self,n):
+        return self.is_p[n]
 
     def first_n_primes(self,n):
         return self.primes[:n] if len(self.primes) >= n else []
