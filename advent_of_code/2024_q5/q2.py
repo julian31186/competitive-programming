18ae94b1f1c44ea290f82aa25a2a854c87123e37
= <julian31186@gmail.com>
2024-12-05 23:46:27 -0500

    pain and suffering

--- advent_of_code/2024_q5/q2.py
+++ advent_of_code/2024_q5/q2.py
@@ -4,13 +4,16 @@ f = open("input.txt")
 
 res = 0
 b = defaultdict(set)
+adj = defaultdict(list)
+rules = []
 
 for i,row in enumerate(f):
     if row == "\n": break
     u,v = [int(x) for x in row.split("|")]
+    rules.append((u,v))
     b[u].add(v)
+    adj[u].append(v)
 
-print(b)
 
 for row in f:
     a,f = [int(x) for x in row.split(",")],True
@@ -18,10 +21,29 @@ for row in f:
         for j in range(i + 1,len(a)):
             if a[i] in b[a[j]]:
                 f = False
-                a[i],a[j] = a[j],a[i]
-    
+                break
+        
+        if not f: break
+
     if not f:
-        res += a[len(a) // 2]
-    
+        indeg = defaultdict(int)
+        for u,v in rules:
+            if u in a and v in a:
+                indeg[v] += 1
+
+        q = deque()
+        fix = []
+        for x in a:
+            if indeg[x] == 0:
+                q.append(x)
+        
+        while q:
+            curr = q.popleft()
+            fix.append(curr)
+            for nei in adj[curr]:
+                indeg[nei] -= 1
+                if indeg[nei] == 0: q.append(nei)
+
+        res += fix[len(fix) // 2]
 
 print(res)
---
+++ advent_of_code/2024_q6/q1.py
@@ -0,0 +1,8 @@
+f = open("input.txt")
+
+res = 0
+for row in f:
+    pass
+
+
+print(res)
