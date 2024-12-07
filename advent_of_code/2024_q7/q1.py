779abe871c59a4512b0956c8dcd1201d90a355a7
= <julian31186@gmail.com>
2024-12-07 00:11:53 -0500

    .

---
+++ advent_of_code/2024_q7/q1.py
@@ -0,0 +1,18 @@
+f = open("input.txt")
+
+res = 0
+
+def dfs(i,acc,goal,nums):
+    if i == len(nums):
+        return acc == goal
+    return dfs(i + 1,acc * nums[i] if acc != 0 else nums[i],goal,nums) or dfs(i + 1,acc + nums[i],goal,nums)
+
+for row in f:
+    row = row.split(":")
+
+    t = int(row[0])
+
+    nums = [int(x) for x in row[1].strip().split(" ")]
+    if dfs(0,0,t,nums): res += t
+
+print(res)
